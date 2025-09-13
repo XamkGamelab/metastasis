aa43a0d1523702343c57c6be846cdbf16d149421
Veera Raita <fvera003@edu.xamk.fi>
2025-09-13 15:31:32 +0300

    Added note full text variable

--- Assets/Scripts/Core/Inventory/ItemScripts/NoteItem.cs
+++ Assets/Scripts/Core/Inventory/ItemScripts/NoteItem.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 namespace SLC.RetroHorror.Core
 {
     [CreateAssetMenu(fileName = "NewNoteItem", menuName = "Items/NoteItem")]
-    public class NoteItem : Item { }
+    public class NoteItem : Item
+    {
+        public string NoteText;
+    }
 }
